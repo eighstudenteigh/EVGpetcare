537f9a6508860d7d147f54b513ba7ea4b88f68cb
dumdumlangs3@gmail.com <dumdumlangs3@gmail.com>
2025-04-01 01:04:38 +0800

    render

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
+use Illuminate\Support\Facades\Log;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +21,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        if (env('APP_ENV') === 'production') { // Force HTTPS in production
+            URL::forceScheme('https');
+        }
     }
 }
