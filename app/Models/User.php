ae6d07c33a7fe9ff7b47e9d53ccae734e809277b
dumdumlangs3@gmail.com <dumdumlangs3@gmail.com>
2025-04-05 16:44:44 +0800

    add forgot pass function

--- app/Models/User.php
+++ app/Models/User.php
@@ -67,7 +67,7 @@ class User extends Authenticatable
     }
     public function sendPasswordResetNotification($token)
 {
-    $this->notify(new ResetPasswordNotification($token));
+    $this->notify(new CustomResetPasswordNotification($token));
 }
     
 }
