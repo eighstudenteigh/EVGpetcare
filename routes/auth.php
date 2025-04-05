877a7af1869bdbca944088b5d289988134800980
dumdumlangs3@gmail.com <dumdumlangs3@gmail.com>
2025-04-05 16:14:39 +0800

    add forgot pass function

--- routes/auth.php
+++ routes/auth.php
@@ -10,27 +10,34 @@ use App\Http\Controllers\Auth\PasswordResetLinkController;
 use App\Http\Controllers\Auth\RegisteredUserController;
 use App\Http\Controllers\Auth\VerifyEmailController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ForgotPasswordController;
+use App\Http\Controllers\Auth\ResetPasswordController;
 
 Route::middleware('guest')->group(function () {
     Route::get('register', [RegisteredUserController::class, 'create'])->name('register');
-
     Route::post('register', [RegisteredUserController::class, 'store']);
 
     Route::get('login', [AuthenticatedSessionController::class, 'create'])->name('login');
-
     Route::post('login', [AuthenticatedSessionController::class, 'store']);
+
+// Forgot Password 
+    Route::get('/forgot-password', [ForgotPasswordController::class, 'showRequestForm'])->name('password.custom.request');
+    Route::post('/forgot-password', [ForgotPasswordController::class, 'sendResetLinkEmail'])->name('password.custom.email');
+//Reset Password    
+    Route::get('/reset-password/{token}', [ResetPasswordController::class, 'showResetForm'])->name('password.reset');
+    Route::post('/reset-password', [ResetPasswordController::class, 'reset'])->name('password.update');
 
 /*     Route::get('forgot-password', [PasswordResetLinkController::class, 'create'])
         ->name('password.request');
 
     Route::post('forgot-password', [PasswordResetLinkController::class, 'store'])
         ->name('password.email'); */
-
+/* 
     Route::get('reset-password/{token}', [NewPasswordController::class, 'create'])
         ->name('password.reset');
 
     Route::post('reset-password', [NewPasswordController::class, 'store'])
-        ->name('password.store');
+        ->name('password.store'); */
 });
 
 Route::middleware('auth')->group(function () {
