05bf229d479da9d58b6dd59cf6d866f508abfc42
dumdumlangs3@gmail.com <dumdumlangs3@gmail.com>
2025-04-07 03:16:21 +0800

    edit forgot pass

--- resources/views/vendor/mail/html/message.blade.php
+++ resources/views/vendor/mail/html/message.blade.php
@@ -3,8 +3,9 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width">
-    <title>{{ $subject }}</title>
+    <title>{{ $slot }}</title>
     <style>
+        /* Laravel's default email styles */
         body {
             font-family: Arial, sans-serif;
             line-height: 1.6;
@@ -13,58 +14,9 @@
             margin: 0 auto;
             padding: 20px;
         }
-        .header {
-            text-align: center;
-            padding: 20px 0;
-            border-bottom: 1px solid #eee;
-            margin-bottom: 30px;
-        }
-        .logo {
-            max-height: 50px;
-        }
-        .content {
-            padding: 20px 0;
-        }
-        .footer {
-            margin-top: 30px;
-            padding-top: 20px;
-            border-top: 1px solid #eee;
-            font-size: 12px;
-            color: #999;
-            text-align: center;
-        }
-        .button {
-            display: inline-block;
-            padding: 10px 20px;
-            background-color: #3490dc;
-            color: white;
-            text-decoration: none;
-            border-radius: 4px;
-            margin: 20px 0;
-        }
     </style>
 </head>
 <body>
-    <div class="header">
-        @if(isset($header))
-            {{ $header }}
-        @else
-            <img src="{{ asset('images/logo.png') }}" alt="Logo" class="logo">
-        @endif
-    </div>
-
-    <div class="content">
-        {{ $slot }}
-    </div>
-
-    <div class="footer">
-        © {{ date('Y') }} {{ config('app.name') }}. All rights reserved.
-        <br>
-        @isset($subcopy)
-            <div class="subcopy">
-                {{ $subcopy }}
-            </div>
-        @endisset
-    </div>
+    {{ $slot }}
 </body>
 </html>
